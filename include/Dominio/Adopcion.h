3df4e6a47681b06d3b07f7cd7ee20a7a99ff7287
constanzaagab04 <162723726+constanzaagab04@users.noreply.github.com>
2025-10-29 10:09:59 -0300

    Update Adopcion.h

--- include/Dominio/Adopcion.h
+++ include/Dominio/Adopcion.h
@@ -6,31 +6,31 @@
 
 class Adopcion {
 private:
-    static int contadorInstancias; // Contador estático [cite: 60]
+    static int contadorInstancias; // Contador estatico 
     int id;
     Animal* animal;
     EstadoAdopcion estado;
     double costoBase;
 
 public:
-    Adopcion(int i, Animal* a, double costo); // Declaración
-    ~Adopcion(); // Declaración
+    Adopcion(int i, Animal* a, double costo); // Declaracion 
+    ~Adopcion(); // Declaracion 
 
     int getId() const { return id; }
     Animal* getAnimal() const { return animal; }
     EstadoAdopcion getEstado() const { return estado; }
     static int getContadorInstancias();
 
-    // Sobrecarga de operador $++$ (Avanzar estado) [cite: 56]
+    // Sobrecarga de operador ++ (Avanzar estado) 
     Adopcion& operator++(); // Declaración
 
-    // Sobrecarga de operador $--$ (Retroceder estado) [cite: 56]
-    Adopcion& operator--(); // Declaración
+    // Sobrecarga de operador -- (retroce estado) 
+    Adopcion& operator--(); // Delcaracion
 
-    // Sobrecarga de operador $()$ para calcular el monto total [cite: 58]
-    double operator()() const; // Declaración
+    // Sobrecarga de operador () para calcular el monto total 
+    double operator()() const; // Declaracion
 
-    // Sobrecarga de operador $<<$ (Impresión) [cite: 54]
+    // Sobrecarga de operador << (Impresión) 
     friend std::ostream& operator<<(std::ostream& os, const Adopcion& ad);
 };
 
